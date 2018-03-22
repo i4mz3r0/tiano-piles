f40dec99ad99803258c519b34456902ab294e7bf
i4mz3r0 <32553761+i4mz3r0@users.noreply.github.com>
2018-03-22 22:02:23 +0100

    Update game.js
    tiles are now randomly generated

--- game.js
+++ game.js
@@ -1,6 +1,29 @@
 class Game{
     constructor(){
         this.started = false
-        this.notes = [["u", -10], ["l", -40], ["r", -70], ["d", -100], ["t", -130]]
-    }
+        let x=[]
+		let y=[]
+		for(let i=0;i<=Math.floor(Math.random()*10)+5;i++){
+			switch(Math.floor((Math.random()*4)+1)){
+				case 0:
+					y=["up",-10-30*i]
+					break
+				case 1:
+					y=["left",-10-30*i]
+					break
+				case 2:
+					y=["right",-10-30*i]
+					break
+				case 3:
+					y=["down",-10-30*i]
+					break
+				case 4: 
+					y=["tap",-10-30*i]
+					break
+			}
+			x.push(y)
+		}
+		console.log(x)
+		this.notes=x	
+	}
 }
